ca35261377a3b9b335465926d60b6709ec6017f4
saifmohammad00 <saifmohammad592001@gmail.com>
2024-09-10 12:57:21 +0530

    final touch to aboutus page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,4 +1,27 @@
-function AboutUs(){
-    return <h1>The About Us Page</h1>
+import Link from "next/link";
+import { Fragment } from "react";
+
+function AboutUs() {
+  const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
+
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
+  return (
+    <Fragment>
+      <h1>The About Us Page</h1>
+      <ul>
+        {details.map((list) => {
+          return (
+            <li key={list.id}>
+              <Link href={`/aboutus/${list.id}`}>{list.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </Fragment>
+  );
 }
 export default AboutUs;
