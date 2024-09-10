f73c7eb3923552313fcc2448c08bab2b44064cde
saifmohammad00 <saifmohammad592001@gmail.com>
2024-09-10 14:44:21 +0530

    dynamic routing

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,11 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails(){
+    return <MeetupDetail 
+    image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+    title="First Meetup"
+    description="This is first meeting"
+    address="some address ,12345 city"
+    />
+}
+export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS=[
+   {id:'m1',
+    title:'A First Meetup',
+    image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address:'Some address 5, 12345 city',
+    description:'This is a First Meetup!'
+   },
+   {id:'m2',
+    title:'A Second Meetup',
+    image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address:'Some address 10, 12345 city',
+    description:'This is a Second Meetup!'
+   }
+];
+
 function HomePage(){
-    return <h1>The Home Page</h1>
+    return <MeetupList meetups={DUMMY_MEETUPS}/>
 }
 export default HomePage;
